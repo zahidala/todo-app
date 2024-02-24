231e82e223e0e67acbe56331696b3efedd02530e
Zahid Allaulddin <zahidalaaudin@gmail.com>
2024-02-24 20:29:37 +0300

    remove unneccessary provider

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,30 @@
 import { Tabs } from 'expo-router'
-import { TodosProvider } from '../../contexts/TodosContext'
 import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 export default function Layout() {
 	return (
-		<TodosProvider>
-			<Tabs>
-				<Tabs.Screen
-					name='all'
-					options={{
-						headerStyle: { backgroundColor: 'blue' },
-						headerTitle: 'All Todos',
-						headerTitleStyle: { color: 'white' },
-						tabBarIcon: () => <FontAwesome name='bars' />,
-						tabBarLabel: 'All Todos',
-					}}
-				/>
+		<Tabs>
+			<Tabs.Screen
+				name='all'
+				options={{
+					headerStyle: { backgroundColor: 'blue' },
+					headerTitle: 'All Todos',
+					headerTitleStyle: { color: 'white' },
+					tabBarIcon: () => <FontAwesome name='bars' />,
+					tabBarLabel: 'All Todos',
+				}}
+			/>
 
-				<Tabs.Screen
-					name='completed'
-					options={{
-						headerStyle: { backgroundColor: 'blue' },
-						headerTitle: 'Completed',
-						headerTitleStyle: { color: 'white' },
-						tabBarIcon: () => <FontAwesome name='check' />,
-						tabBarLabel: 'Completed',
-					}}
-				/>
-			</Tabs>
-		</TodosProvider>
+			<Tabs.Screen
+				name='completed'
+				options={{
+					headerStyle: { backgroundColor: 'blue' },
+					headerTitle: 'Completed',
+					headerTitleStyle: { color: 'white' },
+					tabBarIcon: () => <FontAwesome name='check' />,
+					tabBarLabel: 'Completed',
+				}}
+			/>
+		</Tabs>
 	)
 }
